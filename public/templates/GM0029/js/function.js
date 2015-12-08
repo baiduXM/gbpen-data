e1571514959716bbdf4a296332121b904b189769
ht.5067.org <baidu@eexx.me>
2015-12-08 15:00:38 +0800

    auto

--- public/templates/GM0029/js/function.js
+++ public/templates/GM0029/js/function.js
@@ -1,65 +1,141 @@
-$(document).ready(function() {
-/*侧边栏*/
-		$(".listbg li a").click(function () {
-        $(this).parent().siblings().find(".second").slideUp()
-        $(this).siblings(".second").slideToggle()
-    })  
-    $(".second li a").click(function () {
-        $(this).parent().siblings().find(".third").slideUp()
-        $(this).siblings(".third").slideToggle()
-    }) 
-/*导航*/
-   jQuery("#nav").slide({
-        type: "menu", // 效果类型，针对菜单/导航而引入的参数（默认slide）
-        titCell: ".aa", //鼠标触发对象
-        targetCell: ".cc", //titCell里面包含的要显示/消失的对象
-        effect: "slideDown", //targetCell下拉效果
-        delayTime: 300, //效果时间
-        triggerTime: 0, //鼠标延迟触发时间（默认150）
-        returnDefault: false //鼠标移走后返回默认状态，例如默认频道是"预告片"，鼠标移走后会返回"预告片"（默认false）
+$(document).ready(function(){
+	TouchSlide({ 
+					slideCell:"#slideBox",
+					titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
+					mainCell:".bd ul", 
+					effect:"leftLoop", 
+					autoPage:true,//自动分页
+					autoPlay:true,
+					interTime:6000 //自动播放
+				});
+		TouchSlide({ 
+					slideCell:"#bigimg",
+					mainCell:"#bigimg ul", 
+					effect:"leftLoop", 
+					autoPlay:true,
+					interTime:6000 //自动播放
+				});
+	function floattop(){
+		var menuoff=$(".menu").offset().top;
+		$(".index-wrap").scroll(function(){
+			$(".listleft").height($(".listright").height())
+			var scroH = $(this).scrollTop();
+			if(scroH>menuoff){
+				$(".menu").addClass("menu2")
+				$(".index-wrap").before($(".menu"))
+				
+			}
+			else{
+			$(".menu").removeClass("menu2")
+			$(".baner").before($(".menu"))
+
+			}
+		})
+		}
+	
+
+	$(window).load(function(){
+	floattop();
+	
+	var img=new Image();
+	img.src=$(".relatebox img").attr("src");
+	$(".listleft").height($(".listright").height())
+	$(".class-m").height($(".class").outerHeight()-$(".class-top").outerHeight())
+	$(".index-wrap").height($(".wrap").height())
+	$(".relatepro_m").height($(".relatebox").height()+45)
+	//赖加载
+/* 		$("img.lazy").lazyload({
+		   placeholder : "images/loading.gif",
+		   container:$(".index-wrap"),
+		   effect: "show",
+		   threshold:40
+		}); */
+		
+	});
+	$(window).resize(function(){	
+	floattop();
+		$(".listleft").height($(".listright").height())
+		$(".class-m").height($(".class").outerHeight()-$(".class-top").outerHeight())
+		$(".index-wrap").height($(".wrap").height()+$(".quickbar-tips").height())
+	})
+	function pageSlideOver(){
+    $('.page-out').live('transitionend', function(){
+      $(this).removeClass('page-out');
     });
-
-
-	
-	
-/*产品详细页*/
-    jQuery(".slideBoxb").slide({
-        mainCell: ".bd ul",
-        autoPlay: false,
-        effect: "leftLoop"
+    $('.page-in').live('transitionend', function(){
+      $(this).removeClass('page-in');
     });
+  }
+  //隐藏导航跟wrap的切换
+	$("#class").click(function(){
+		$(".class").removeClass("page-prev").addClass("page-in");
+		$(".wrap").removeClass("page-active").addClass("page-next page-in")
+		$(".opacity2").show()
+		pageSlideOver();
+		
+	})
+	$(".class-close,.opacity2").on('touchstart',function(){
+		$(".class").addClass("page-prev page-out")
+		$(".wrap").removeClass("page-next").addClass(" page-out")
+		$(".opacity2").hide()
+		$(".newsclass").removeClass("show")
+		pageSlideOver();
+		return false;
+	})
 	
+	$(".tell-icon .close").click(function(){
+		$(".tell-icon").removeClass("display-block")
+		$(".index-wrap").height($(window).height()-$(".fixed").height())
+		return false
+	})
 	
- // 兼容低版本IE
-    $(function() {
-        if (window.PIE) {
-            $('.rounded').each(function() {
-                PIE.attach(this);
-            });
-        }
-    });
+	//字体上下拉开始
+  	
+	$("#font").click(function(){
+		$("#up").hide()
+		$("#down").show()
+		$(".hide-class").removeClass("hide-class1")
+		$(".font").slideToggle()
+		$(".hide-class").slideUp()	
+	})
+	$(".font dl.big").click(function(){
+		$(".edite").attr('class',"edite font-big")
+		$(".news-ins").attr('class',"news-ins font-big")
+		$(".font").hide()
+	})
+	$(".font dl.normal").click(function(){
+		$(".edite").attr('class',"edite font-normal")
+		$(".news-ins").attr('class',"news-ins font-normal")
+		$(".font").hide()
+	})
+	$(".font dl.small").click(function(){
+		$(".edite").attr('class',"edite font-small")
+		$(".news-ins").attr('class',"news-ins font-normal")
+		$(".font").hide()
 
-/*大图 100%*/	
-	$("#kinMaxShow").kinMaxShow({
-            height: 350,
-            button: {
-                showIndex: false,
-                normal: { background: 'url(images/button.png) no-repeat -14px 0', marginRight: '8px', border: '0', right: '48%', bottom: '20px' },
-                focus: { background: 'url(images/button.png) no-repeat 0 0', border: '0' }
-            }
-        });
+	})	
+	//字体上下拉结束
+	//分类上下拉开始
+	$("#first").click(function(){
+		$("#up").toggle()
+		$("#down").toggle()
+		$(".hide-class").slideToggle()
+		$(".font").slideUp()	
+	})
+	//分类上下拉结束
 	
-/*滚动js*/	
-var MarqueeDiv1Control=new Marquee("MarqueeDiv1");		//箭头控制滚动方向、加速及鼠标拖动实例
-MarqueeDiv1Control.Direction="left";
-MarqueeDiv1Control.Step=1;
-MarqueeDiv1Control.Width=950;
-//MarqueeDiv1Control.Height=167;
-MarqueeDiv1Control.Timer=20;
-MarqueeDiv1Control.ScrollStep=1;				//若为-1则禁止鼠标控制实例
-MarqueeDiv1Control.Start();
-MarqueeDiv1Control.BakStep=MarqueeDiv1Control.Step;
-
-
-
-});
+	//返回顶部开始
+	$(".back-top").click(function(){$(".index-wrap").animate({scrollTop :0}, 800)})
+	$("#share_btn").click(function(){
+		$(".newsclass").addClass("show")
+		$(".opacity2").show()
+		return false
+	})
+	//返回顶部结束
+	//分享取消按钮开始
+	$(".share-cance").click(function(){
+		$(".newsclass").removeClass("show")
+		$(".opacity2").hide()
+	})
+  	});
+	//分享取消按钮结束
