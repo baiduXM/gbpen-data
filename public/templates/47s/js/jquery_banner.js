7951a40cfc8e139ab9aeff6c21fb1ba79c7d3ab4
f.no02.gbpen.cn <153@baidu>
2016-01-05 11:00:09 +0800

    auto

---
+++ public/templates/47s/js/jquery_banner.js
@@ -0,0 +1,4 @@
+jQuery("#slideBox").slide({mainCell:".bd ul",autoPlay:true,effect:"left"});
+//banner
+jQuery("#slide").slide({mainCell:".bd ul",autoPlay:true,effect:"left"});
+jQuery("#slide").hover(function(){ jQuery(this).find(".prev,.next") });
