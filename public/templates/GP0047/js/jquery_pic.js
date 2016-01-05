3d96362d29d16bc3a4a72b8ee087847b14d237e7
f.no02.gbpen.cn <153@baidu>
2016-01-05 15:30:09 +0800

    auto

--- public/templates/GP0047/js/jquery_pic.js
+++ public/templates/GP0047/js/jquery_pic.js
@@ -1,11 +1,5 @@
-
-jQuery(".hxz").slide({mainCell:".bd ul",autoPlay:true,effect:"leftMarquee",vis:5,interTime:50});
-
-jQuery(".pro_det").slide({mainCell:"ul",autoPlay:true,effect:"leftMarquee",vis:4,interTime:50});
-
+alert(11)
 jQuery('.gd').slide({mainCell:"ul.picList",autoPlay:true,effect:"leftMarquee",vis:4,interTime:50});
-
-jQuery('.picMarquee').slide({mainCell:"ul.picList",autoPlay:false,effect:"leftMarquee",vis:4,interTime:50});
 
 
 
--- public/templates/GP0047/js/left_nav.js
+++ public/templates/GP0047/js/left_nav.js
@@ -1,11 +1,8 @@
-
- $(document).ready(function($) {
- 	 var a=$(".current").children('a')
-
-
- 	a.click(function() {
- 		
- 		$(this).next().slideToggle()
-
- 	});
- });
+$(document).ready(function($) {
+    var a = $("li.current").children('a')
+    a.click(function() {
+        $(this).next().slideToggle(400,function(){
+        	$(this).is(':hidden') ? $(this).parent().css('background','url(images/sj_31.jpg) no-repeat 80% center') : $(this).parent().css('background','none');
+        })
+    });
+});
