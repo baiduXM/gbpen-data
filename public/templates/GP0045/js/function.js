e24bf87b7f8094c915ff4a5fdee54b0dab125369
f.no02.gbpen.cn <153@baidu>
2016-01-05 10:00:12 +0800

    auto

--- public/templates/GP0045/js/function.js
+++ public/templates/GP0045/js/function.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 	// banner
 	jQuery(".focusBox").slide({ mainCell:".pic",effect:"left", autoPlay:true, delayTime:500,interTime:4000,trigger:"click"});
-
-
+	jQuery(".pimgw").slide({mainCell:".bd ul",autoPlay:true,effect:"left"});
 	// 产品展示
 	$('#prizes .photo1').jCarouselLite({
 	      btnPrev: '#prizes a.photos-prev',
@@ -19,12 +18,25 @@ $(document).ready(function() {
 	      auto: 3000,
 	      speed: 1000
 	}).css({visibility:"visible"});
-
+	// 导航显隐
+	$(".main_nav-percent li a").mouseenter(function(event) {
+		var _this = $(this);
+        timer = setTimeout(function(){
+        	var thischild = _this.siblings('ul');console.log(thischild);
+        	if(thischild.length != 0){
+	        	thischild.fadeIn();
+	        }
+        }, 400)
+	});
+    $(".main_nav-percent li").unbind('mouseleave').mouseleave(function(event) {
+    	clearTimeout(timer);
+        $(this).find('.sub').fadeOut();
+    });
 // 内页
-	$(".listbg li a").click(function(){		
-		$(this).next(".second").slideToggle(300);
-		return false;
-		})	
-
-
+	$(".listbg li a").click(function(){	
+	 	if($(this).next(".second").length > 0){
+	 		$(this).next(".second").slideToggle(300);
+	 		return false;
+	 	}
+	})	
 });
