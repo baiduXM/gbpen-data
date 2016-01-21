838fa86249d7de2cfb4b0493ba51f5fd1aa24acf
f.no02.gbpen.cn <153@baidu>
2016-01-21 18:00:11 +0800

    auto

--- public/templates/GM0046/js/function.js
+++ public/templates/GM0046/js/function.js
@@ -1,51 +1,26 @@
-$(document).ready(function(){
-	TouchSlide({ 
-					slideCell:"#slideBox",
-					titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
-					mainCell:".bd ul", 
-					effect:"leftLoop", 
-					autoPage:true,//自动分页
-					autoPlay:1000,
-					interTime:6000 //自动播放
-				});
-	$(".index-wrap").height($(window).height()-$(".fixed").height())
-	$(".class-m").height($(".class").outerHeight()-$(".class-top").outerHeight())
-	$(window).resize(function(){
-		$(".class-m").height($(".class").outerHeight()-$(".class-top").outerHeight())
-		$(".index-wrap").height($(window).height()-$(".fixed").height())
-	})
-	$("#share_btn").click(function(){
-	 $(".share-con").slideToggle(100)
-	});
-	//隐藏导航跟wrap的切换
-	$("#class").click(function(){
-		$(".class").addClass("hover")
-		$(".wrap").addClass("hover1")
-		$(".opacity2").show()
-		$("body,html").addClass("overflow")
-	})
-	$(".class-close,.opacity2").click(function(){
-		$(".class").removeClass("hover")
-		$(".wrap").removeClass("hover1")
-		$(".opacity2").hide()
-		$("body,html").removeClass("overflow")
-	})
-  	$("#first").click(function(){
-		$("#up,#down").toggle()
-		$(".hide-class").slideToggle()
-	})
-	$("#font").click(function(){
-		$(".font").slideToggle()
-	})
-	
-  //幻灯片元素与类"menu_body"段与类"menu_head"时点击
-	$(".menu_head .icon").click(function(){
-		$(this).parent().toggleClass("cur").next(".menu_body").slideToggle(300).siblings(".menu_body").slideUp("slow");
-	$(this).parent().siblings().removeClass("cur");
-	});
-	//返回定都
-	$('.back-top').click(function(){
-		$('.index-wrap .scroll-container').find('.swiper-wrapper').css({'transform':'translate3d(0px, 0px, 0px)','-webkit-transform':'translate3d(0px, 0px, 0px)','transition': '0.3s'});
+jQuery(document).ready(function(){
 
-	})
-  });
+
+
+    // jQuery(".fullSlide").slide({ titCell:".hd ul", mainCell:".bd ul", effect:"fold",  autoPlay:true, autoPage:true, trigger:"click" });
+
+// $('.ny-prolist li:eq(2)').css('marginRight',0);
+// $('.ny-prolist li:eq(5)').css('marginRight',0);
+// $('.ny-prolist li:eq(8)').css('marginRight',0);
+
+$('.ny-prolist li:nth-child(3n)').css('marginRight',0);
+	$('.ny-prolist li a').hover(function() {
+			$(this).children('h2').show();
+		}, function() {
+			$(this).children('h2').hide();
+		});
+// 内页
+	// $(".first li a").click(function(){		
+	// 	$(this).next(".second").slideToggle(300);
+	// 	// return false;
+	// 	})	
+
+
+
+
+});
