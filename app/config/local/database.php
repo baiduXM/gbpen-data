b58829e9fea7ff1555bfd674912c48577a2398d0
Esone <01@eexx.me>
2016-01-28 14:28:24 +0800

    测试服务器迁移

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '59.56.69.154',
+			'host'      => '182.61.21.188',
 			'database'  => 'unify',
 			'username'  => 'admin',
 			'password'  => 'admin12t',
