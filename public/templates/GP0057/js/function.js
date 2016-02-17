e1903b9bfe408a691ce4de8dce48775ac4fe582a
ht.5067.org <baidu@eexx.me>
2016-02-17 18:00:07 +0800

    auto

--- public/templates/GP0057/js/function.js
+++ public/templates/GP0057/js/function.js
@@ -123,7 +123,7 @@ $(document).ready(function() {
 		$('#prizes .photos-content.enable').jCarouselLite({
 			btnPrev: '#prizes a.photos-prev',
 			btnNext: '#prizes a.photos-next',
-			visible: 6,
+			visible:3,
 			auto: 3000,
 			speed: 1000
 		}).css({visibility:"visible"});
