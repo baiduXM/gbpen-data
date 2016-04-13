962cbf0e33041c5c01f92a9e9bf4e6411266453f
ht.5067.org <baidu@eexx.me>
2016-04-13 16:33:30 +0800

    auto

--- public/templates/GP0030/js/function.js
+++ public/templates/GP0030/js/function.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
 $("#header").slideDown("slow")
 $("#banner").slideDown("slow")
 	$("#kinMaxShow").kinMaxShow({
-            height: 450,
+            height: 582,
             button: {
                 showIndex: false,
                 normal: { background: 'url(images/button.png) no-repeat -14px 0', marginRight: '8px', border: '0', right: '48%', bottom: '20px' },
