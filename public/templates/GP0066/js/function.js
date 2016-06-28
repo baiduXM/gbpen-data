0107b4b83787ee9a85bb3a0e25be3dba3c1239a3
ht.5067.org <baidu@eexx.me>
2016-06-28 14:03:46 +0800

    auto

--- public/templates/GP0066/js/function.js
+++ public/templates/GP0066/js/function.js
@@ -1,6 +1,34 @@
 jQuery(document).ready(function($) {
 	
 
+jQuery(".slideBoxb").slide({mainCell:".bd ul",autoPlay:false,effect:"leftLoop"});
+
+jQuery(".slider").slide({ titCell:".hd ul", mainCell:".bd ul", effect:"left",  autoPlay:true, autoPage:true, trigger:"click",
+			mouseOverStop:false,/* 鼠标移到容器层继续播放*/
+			
+		});
+
+(function(){
+			var ind = 2; //初始位置
+			var nav= jQuery(".nav");
+			// var init = jQuery(".nav .m").eq(ind);
+			var block = jQuery(".nav .block"); //滑块
+			// block.css({"left":init.position().left-3}); //初始化滑块位置
+			// nav.hover(function(){},function(){ block.animate({"left":init.position().left-3},100); }); //移出导航滑块返回
+
+			jQuery(".nav").slide({ 
+					type:"menu", //效果类型
+					titCell:".m", // 鼠标触发对象
+					targetCell:".sub", // 效果对象，必须被titCell包含
+					delayTime:300, // 效果时间
+					triggerTime:0, //鼠标延迟触发时间
+					returnDefault:true,//on返回初始位置
+					defaultIndex:ind,//初始位置
+					startFun:function(i,c,s,tit){ //控制当前滑块位置
+						block.animate({"left":tit.eq(i).position().left-3},100);
+					}
+				});
+	})()
 
 nav(".aboutNav");
 	function nav(stair){
@@ -44,36 +72,13 @@ nav(".aboutNav");
 	// <!-- 导航 -->
 
 
-	(function(){
-			var ind = 2; //初始位置
-			var nav= jQuery(".nav");
-			var init = jQuery(".nav .m").eq(ind);
-			var block = jQuery(".nav .block"); //滑块
-			block.css({"left":init.position().left-3}); //初始化滑块位置
-			nav.hover(function(){},function(){ block.animate({"left":init.position().left-3},100); }); //移出导航滑块返回
-
-			jQuery(".nav").slide({ 
-					type:"menu", //效果类型
-					titCell:".m", // 鼠标触发对象
-					targetCell:".sub", // 效果对象，必须被titCell包含
-					delayTime:300, // 效果时间
-					triggerTime:0, //鼠标延迟触发时间
-					returnDefault:true,//on返回初始位置
-					defaultIndex:ind,//初始位置
-					startFun:function(i,c,s,tit){ //控制当前滑块位置
-						block.animate({"left":tit.eq(i).position().left-3},100);
-					}
-				});
-	})()
+	
 
 
 
 // <!-- banner切换 -->
 
-		jQuery(".slider").slide({ titCell:".hd ul", mainCell:".bd ul", effect:"left",  autoPlay:true, autoPage:true, trigger:"click",
-			mouseOverStop:false,/* 鼠标移到容器层继续播放*/
-			
-		});
+		
 
 
 // <!-- 图片滚动 -->
@@ -82,7 +87,7 @@ jQuery(".picScroll").slide({ mainCell:"ul", effect:"leftMarquee", vis:4, autoPla
 
 
 /*鼠标移过，左右按钮显示*/
-jQuery(".focusBox").hover(function(){ jQuery(this).find(".prev,.next").stop(true,true).fadeTo("show",0.2) },function(){ jQuery(this).find(".prev,.next").fadeOut() });
+// jQuery(".focusBox").hover(function(){ jQuery(this).find(".prev,.next").stop(true,true).fadeTo("show",0.2) },function(){ jQuery(this).find(".prev,.next").fadeOut() });
 /*SuperSlide图片切换*/
-jQuery(".focusBox").slide({ mainCell:".pic",effect:"fold", autoPlay:true, delayTime:600, trigger:"click"});
+// jQuery(".focusBox").slide({ mainCell:".pic",effect:"fold", autoPlay:true, delayTime:600, trigger:"click"});
 
