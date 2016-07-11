ca3207178a130b7b483e8ce101564a46aae568c6
ht.5067.org <baidu@eexx.me>
2016-07-11 10:11:20 +0800

    auto

--- public/templates/GP0079/js/function.js
+++ public/templates/GP0079/js/function.js
@@ -32,9 +32,9 @@ $(document).ready(function() {
 	});				
 	jQuery(".focusBoxa").slide({ titCell:".num li", mainCell:".pic",effect:"fold", autoPlay:true,trigger:"click",
 		//下面startFun代码用于控制文字上下切换
-		startFun:function(i){
-			 jQuery(".focusBoxa .txt li").eq(i).animate({"bottom":0}).siblings().animate({"bottom":-36});
-		}
+		// startFun:function(i){
+		// 	 jQuery(".focusBoxa .txt li").eq(i).animate({"bottom":0}).siblings().animate({"bottom":-36});
+		// }
 	});	
 	jQuery(".slideTxtBox").slide();		
 		$('#prizes .photos-content.enable').jCarouselLite({
