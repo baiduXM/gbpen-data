ed804f9b7867b6e6b2ca740509651e03218ce249
ht.5067.org <baidu@eexx.me>
2016-07-16 11:51:10 +0800

    auto

--- public/templates/GM0098/js/function.js
+++ public/templates/GM0098/js/function.js
@@ -167,9 +167,8 @@ jQuery(document).ready(function($) {
 		});
 		
 		//首页产品pro_top的高度
-		/*var abouth=$(".pro_top img").height();
-		//$(".pro_top img").css('float','left');
-		$(".pro_dt").outerHeight(abouth);
+		var abouth=$(".pro_top img").height();
+		//$(".pro_dt").outerHeight(abouth);
 		console.log($(".pro_top img"));
-		console.log(abouth);*/
+		console.log(abouth);
 });
