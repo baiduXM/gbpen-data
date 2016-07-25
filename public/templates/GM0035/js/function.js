b82e1978b479ccf225d29baf1c6a9d0c9422d7dc
ht.5067.org <baidu@eexx.me>
2016-07-25 18:00:12 +0800

    auto

--- public/templates/GM0035/js/function.js
+++ public/templates/GM0035/js/function.js
@@ -1,41 +1,52 @@
 $(document).ready(function(){
 
-	// $(window).load(function(){
-	// var img=new Image();
-	// img.src=$(".about_img img").attr("src");
-	// img.src=$(".fenleishowr img").attr("src");	
-	// img.src=$(".nimg1 img").attr("src");
-	// img.src=$(".nimg2 img").attr("src");			
-	// $(".class-m").height($(".class").outerHeight()-$(".class-top").outerHeight())
-	// $(".index-wrap").height($(".wrap").height()-$(".fixed").height()-$(".tell-icon").height())
-	// $(".mlist").height($(".mimg").height())
-	// $(".mlist_null").height($(".mimg").height())
-	// if($(window).width()>320&&$(window).width()<500){
-	// 	$(".box_menu").height($(".index-wrap").height()-$(".header").height()-$(".foot").height()-46)
-	// }else{
-	// 	$(".box_menu").height($(".index-wrap").height()-$(".header").height()-$(".foot").height()+20)
-	// 	}		 		
-	// });
+	TouchSlide({ 
+				slideCell:"#slideBox",
+				titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
+				mainCell:".bd ul", 
+				effect:"leftLoop", 
+				autoPage:true,//自动分页
+				autoPlay:true,
+				interTime:6000 //自动播放
+			});
 	
-	// $(window).resize(function(){	
-	// $(".mlist").height($(".mimg").height())
-	// $(".mlist_null").height($(".mimg").height())
-	// if($(window).width()>320&&$(window).width()<500){
-	// 	$(".box_menu").height($(".index-wrap").height()-$(".header").height()-$(".foot").height()-46)
-	// }else{
-	// 	$(".box_menu").height($(".index-wrap").height()-$(".header").height()-$(".foot").height()+20)
-	// 	}					
-	// })
-	$(".class-m").height($(".class").outerHeight()-$(".class-top").outerHeight())
-	$(".index-wrap").height($(".wrap").height()-$(".fixed").height()-$(".tell-icon").height())	
+	$(window).load(function(){
+		// var img=new Image();
+		// img.src=$(".about_img img").attr("src");
+		// img.src=$(".fenleishowr img").attr("src");	
+		// img.src=$(".nimg1 img").attr("src");
+		// img.src=$(".nimg2 img").attr("src");			
+		$(".class-m").height($(".class").outerHeight()-$(".class-top").outerHeight())
+		$(".index-wrap").height($(".wrap").height()-$(".fixed").height()-$(".tell-icon").height())
+	    //	$(".box_menu").height($(".index-wrap").height()-$(".header").height()-$(".foot").height()-46)
+		$(".mlist").height($(".mimg").height())
+		$(".mlist_null").height($(".mimg").height())
+		if($(window).width()>320&&$(window).width()<500){
+			$(".box_menu").height($(".index-wrap").height()-$(".header").height()-$(".foot").height()-46)
+		}else{
+			$(".box_menu").height($(".index-wrap").height()-$(".header").height()-$(".foot").height()+20)
+			}		 		
+	});
+	
+	$(window).resize(function(){	
+		$(".mlist").height($(".mimg").height())
+		$(".mlist_null").height($(".mimg").height())
+	 	//$(".box_menu").height($(".index-wrap").height()-$(".header").height()-$(".foot").height()-46)
+		if($(window).width()>320&&$(window).width()<500){
+			$(".box_menu").height($(".index-wrap").height()-$(".header").height()-$(".foot").height()-46)
+		}else{
+			$(".box_menu").height($(".index-wrap").height()-$(".header").height()-$(".foot").height()+20)
+			}					
+	})
+	
 	function pageSlideOver(){
-    $('.page-out').live('transitionend', function(){
-      $(this).removeClass('page-out');
-    });
-    $('.page-in').live('transitionend', function(){
-      $(this).removeClass('page-in');
-    });
-  }
+	    $('.page-out').live('transitionend', function(){
+	      $(this).removeClass('page-out');
+	    });
+	    $('.page-in').live('transitionend', function(){
+	      $(this).removeClass('page-in');
+	    });
+    }
   //隐藏导航跟wrap的切换
   $("#daohang").click(function(){
 		$(".class").removeClass("page-prev").addClass("page-in");
@@ -53,13 +64,15 @@ $(document).ready(function(){
 	})
 	$(".class-close,.opacity2").on('touchstart',function(){
 		$(".class").addClass("page-prev page-out")
-		$(".wrap").removeClass("page-next").addClass(" page-out")
+		$(".wrap").removeClass("page-next").addClass("page-out")
 		$(".opacity2").hide()
 		$(".newsclass").removeClass("show")
 		pageSlideOver();
 		
 	})
 	
+
+
 	$(".tell-icon .close").click(function(){
 		$(".tell-icon").removeClass("display-block")
 		$(".index-wrap").height($(window).height()-$(".fixed").height())
@@ -107,15 +120,8 @@ $(document).ready(function(){
 		$(".font").slideUp()	
 	})
 	
-	TouchSlide({ 
-					slideCell:"#slideBox",
-					titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
-					mainCell:".bd ul", 
-					effect:"leftLoop", 
-					autoPage:true,//自动分页
-					autoPlay:true,
-					interTime:6000 //自动播放
-				});				 
+
+			 
 				 					                            		
 	
   });
