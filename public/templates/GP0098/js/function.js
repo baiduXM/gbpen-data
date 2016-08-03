0c1a148092093635f447b90f3721e442a04d9682
ht.5067.org <baidu@eexx.me>
2016-08-03 16:44:37 +0800

    auto

--- public/templates/GP0098/js/function.js
+++ public/templates/GP0098/js/function.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
     jQuery("#nav").slide({ type:"menu", titCell:".nLi", targetCell:".sub",effect:"slideDown",delayTime:300,triggerTime:0,returnDefault:true});
-	jQuery(".slideBox").slide({mainCell:".bd ul",autoPlay:true, effect:"fold", trigger:"click",interTime:4000});
+	  // jQuery(".slideBox").slide({mainCell:".bd ul",autoPlay:true, effect:"fold", trigger:"click",interTime:4000});
     jQuery(".picMarquee-left").slide({mainCell:".bd ul",autoPlay:true,effect:"leftLoop",vis:6,interTime:3000,prevCell:'.prev',nextCell:'.next'});
     $('ul.nav>li:last-child,ul.nav>li:last-child h3').css('border',"none")
 
