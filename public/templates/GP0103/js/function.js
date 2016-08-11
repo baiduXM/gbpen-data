c447cf0e6bc9bf9979a1019cba5373f3008997b9
ht.5067.org <baidu@eexx.me>
2016-08-11 18:00:13 +0800

    auto

--- public/templates/GP0103/js/function.js
+++ public/templates/GP0103/js/function.js
@@ -1,69 +1,117 @@
 
-$(document).ready(function(){
 
-//下拉菜单
-    jQuery(".navbar").slide({ 
-        type:"menu",// 效果类型，针对菜单/导航而引入的参数（默认slide）
-        titCell:".nav_li", //鼠标触发对象
-        targetCell:".sub", //titCell里面包含的要显示/消失的对象
-        effect:"slideDown", //targetCell下拉效果
-        delayTime:100 , //效果时间
-        triggerTime:0, //鼠标延迟触发时间（默认150）
-        returnDefault:true //鼠标移走后返回默认状态，例如默认频道是“预告片”，鼠标移走后会返回“预告片”（默认false）
-      });
-
- /*鼠标移过，左右按钮显示*/
-jQuery(".focusBoxa").hover(function(){ jQuery(this).find(".prev,.next").stop(true,true).fadeTo("show",1) },function(){ jQuery(this).find(".prev,.next").fadeOut() });
-
-/*SuperSlide图片切换*/
-jQuery(".focusBoxa").slide({ mainCell:".pic",titCell:".hd li",effect:"fold", autoPlay:true, delayTime:600, trigger:"click"});
-
-//产品展示轮播
-jQuery(".slideBox1").slide({ mainCell:" .ulWrap",autoPage:true,interTime:50,effect:"leftMarquee",autoPlay:true,trigger:"click",vis:4});
+window.onload=function(){
+	<!--美工-->
 
 
-jQuery(".slideBox2").slide({ mainCell:" .ulWrap",autoPage:true,interTime:50,effect:"leftMarquee",autoPlay:true,trigger:"click",vis:6});
+$("#about ").each(
+ function(){
+	 $(this).find(".about_text").width($(this).width()-$(this).find(".about_img img").width()-25)
+	 }
+ )
+	
+
+$("#banner .slideBox").slide({mainCell:".bd ul",autoPlay:true, effect:"fold", trigger:"click",interTime:4000});
+
+$("#products .inner ").slide({mainCell:".bd ul",autoPlay:true,effect:"leftMarquee",vis:6,interTime:50});
 
 
-/*内页三级菜单下拉*/
-    jQuery(".secondul").slide({
-      titCell:".sub2", //鼠标触发对象
-      targetCell:".sub2 ul", //与titCell一一对应，第n个titCell控制第n个targetCell的显示隐藏
-      effect:"slideDown", //targetCell下拉效果
-      delayTime:300 , //效果时间
-      triggerTime:0, //鼠标延迟触发时间（默认150）
-      defaultPlay:true,//默认是否执行效果（默认true）
-      returnDefault:true 
-      });
-    jQuery(".firstul").slide({
-      titCell:".sub1", //鼠标触发对象
-      targetCell:".secondul", //与titCell一一对应，第n个titCell控制第n个targetCell的显示隐藏
-      effect:"slideDown", //targetCell下拉效果
-      delayTime:300 , //效果时间
-      triggerTime:0, //鼠标延迟触发时间（默认150）
-      defaultPlay:true,//默认是否执行效果（默认true）
-      returnDefault:true 
-      });
+	
+	
 
-//多图时轮播
-    jQuery(".slideBox3").slide({  mainCell:".bd ul", effect:"left", delayTime:1000, autoPlay:true, autoPage:true, trigger:"click",
-    mouseOverStop:false,/* 鼠标移到容器层继续播放*/
-    /* 控制进度条 */
-      startFun:function(){
-        var timer = jQuery(".slideBox3 .timer");
-        timer.stop(true,true).animate({ "width":"0%" },0).animate({ "width":"100%" },1000);
-      }
+function adaption(selector,li_w,li_h){
+		$(selector).each(function(i){
+		img_i=$(selector).eq(i).find("img")
+	    if(img_i.width() >= li_w&&img_i.width()>=img_i.height()){
+          img_i.width("100%");}
+        else if(img_i.height() > li_h){
+         img_i.height("100%");}
+	   var img_h=img_i.height()	
+       var img_w=img_i.width()												
+       var mar_t=Math.floor((li_h-img_h)*0.5);
+       var mar_l=Math.floor((li_w-img_w)*0.5);
+  	   $(this).css({"padding-top":mar_t+"px","padding-left":mar_l+"px"})
+	   $(this).width($(this).width()-mar_l);
+	   $(this).height($(this).height()-mar_t);
+		})
+	}
+/*首页产品列表图片自适应*/
+adaption("#products .inner ul li .pic",130,130)
+
+
+
+
+	 
+/*图文内页图片自适应*/	 
+ $(".list_imagetext ul li").each(
+ function(){
+	 var wid1=$(this).find(".img_box img").width();
+	 var wid2=$(this).width();
+	 $(this).find(".text_box").width(wid2-wid1-11)
+	 }
+ )
+ 
+
+  
+/*	 $(".yc ul li").each(function(){
+		 var ww=$(this).find("img").width();
+   $(this).find("p").width(ww);
+  });*/
+  
+}
+
+$(document).ready(function() {
+	
+
+/*产品详细页图片自适应*/
+	$(".bd li").each(function(i){
+	if($(".bd li").eq(i).find("img").width() > 640){
+       $(".bd li").eq(i).find("img").width("99%");
+}
+
+		})
+
+	
+/*侧边栏*/
+		$(".listbg li a").click(function () {
+        $(this).parent().siblings().find(".second").slideUp()
+        $(this).siblings(".second").slideToggle()
+    })  
+    $(".second li a").click(function () {
+        $(this).parent().siblings().find(".third").slideUp()
+        $(this).siblings(".third").slideToggle()
+    }) 
+	
+/*导航*/
+   jQuery("#nav").slide({
+        type: "menu", // 效果类型，针对菜单/导航而引入的参数（默认slide）
+        titCell: ".aa", //鼠标触发对象
+        targetCell: ".cc", //titCell里面包含的要显示/消失的对象
+        effect: "slideDown", //targetCell下拉效果
+        delayTime: 300, //效果时间
+        triggerTime: 0, //鼠标延迟触发时间（默认150）
+        returnDefault: false //鼠标移走后返回默认状态，例如默认频道是"预告片"，鼠标移走后会返回"预告片"（默认false）
     });
 
-//限制字符个数
-    $(".h3_l").each(function(){
-    var maxwidth=parseInt($(this).attr("data-limit"));
-    if($(this).text().length>maxwidth){
-    $(this).text($(this).text().substring(0,maxwidth));
-    $(this).html($(this).html()+'...');
-    }
+	
+	
+/*产品详细页*/
+    jQuery(".slideBoxb").slide({
+        mainCell: ".bd ul",
+        autoPlay: false,
+        effect: "leftLoop"
+    });
+	
+	
+ // 兼容低版本IE
+    $(function() {
+        if (window.PIE) {
+            $('.rounded').each(function() {
+                PIE.attach(this);
+            });
+        }
     });
 
 
-        
+
 });
