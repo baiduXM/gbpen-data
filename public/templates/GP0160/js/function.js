bb0b149d26667f7549d736c240bdd1f1f0e2931e
ht.5067.org <baidu@eexx.me>
2016-09-23 18:00:14 +0800

    auto

--- public/templates/GP0160/js/function.js
+++ public/templates/GP0160/js/function.js
@@ -1,18 +1,31 @@
-jQuery(document).ready(function(){
+$(document).ready(function(){
+
+    // banner滚动
+  	jQuery(".slideBox").slide({mainCell:".banimg ul",autoPlay:true});
+  	
+   $(".navs ul li").hover(function(){   
+
+		$(this).find(".erji").stop(false,true).slideDown(200);
+		},function(){ 
+		$(this).find(".erji").stop(false,true).slideUp("");
+		})
 
 
+   jQuery(".picScroll-left").slide({mainCell:".bd ul",autoPage:true,effect:"left",
+               autoPlay:true,vis:1,trigger:"click"});
+    
+	// 导航当前状态
+    var num=window.location.href.split("/").length-1;    
+    var index=window.location.href.split("/")[num];
+
+    if(index==null || index==0){
+    	$("ul.menu>li:eq(0)>a").addClass('in')
+    }else{
+    	$("ul.menu>li>a[href^='"+index+"']").addClass('in')
+    }
+});	
+	
+    
 
 
-$('.ny-prolist li:nth-child(4n)').css('marginRight',0);
-	$('.ny-prolist li a').hover(function() {
-			$(this).children('h2').show();
-		}, function() {
-			$(this).children('h2').hide();
-		});
-
-
-
-
-
-});
-
+	
