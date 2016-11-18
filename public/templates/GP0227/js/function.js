c514afccf01b45d1e40b10da11e41aa70a8581b6
bdxm <baidu@eexx.me>
2016-11-18 15:42:19 +0800

    auto

--- public/templates/GP0227/js/function.js
+++ public/templates/GP0227/js/function.js
@@ -1,69 +1,62 @@
+
 $(document).ready(function() {
 
+// 导航
+	$(".menu li").hover(function() {
+		var ulLeft = ($(this).width() - $(this).find('.menu-list').width())/2
+		$(this).find('.menu-list').css({left:ulLeft}).stop(true, true).slideDown(300);
+	}, function() {
+		$(this).find('.menu-list').slideUp(300);
+	});
 
-	 // nav
-     jQuery("#nav").slide({ type:"menu", titCell:".nLi", targetCell:".sub",effect:"slideDown",delayTime:300,triggerTime:0,returnDefault:true});
+// 焦点图片切换
+	jQuery("#banner").slide({ titCell:".dot ol", mainCell:"ul.picture", effect:"left",  autoPlay:true, autoPage:true, trigger:"click" });
 
-	// banner js
-		jQuery(".slideBox").slide({mainCell:".bd ul",autoPlay:true, effect:"fold", trigger:"click",interTime:4000});
-	// pro js
-		jQuery(".picMarquee-left").slide({mainCell:".bd ul",autoPlay:true,effect:"leftMarquee",vis:3,interTime:50});
+// 编辑框
+	$("ul.first a").click(function(event) {
+		if ($(this).nextAll("ul").length > 0) {
+			var a=$(this);
+			var thisname = a.attr('class');
+			if (thisname == null || thisname == 0) {
+				a.nextAll("ul").slideDown(300);
+				a.parent().siblings().find('ul').slideUp(500).prevAll("a").removeClass();
+				var parent = a.parent().parent("ul").attr('class');
+				switch (parent) {
+					case "first":
+						a.addClass("box-on");
+						break;
+					case "second":
+						a.addClass("box-in");
+						break;
+					default:
+						a.addClass("box_at");
+				}
+			}else{
+				a.removeClass().nextAll('ul').slideUp(300);
+				a.nextAll('ul').find('a').removeClass().nextAll('ul').slideUp(300);
+				a.next("i").removeClass('in');
+			};
+			return false;
+		};
+	});
 
-// 返回顶部
-		$(".back").click(function(){$("html,body").animate({scrollTop :0}, 800)})
-		// $('.morebtn a').hover(function() {
-		// 	$(this).prev('h2').hide();
-		// }, function() {
-		// 	$(this).prev('h2').show();
-		// });
+// // 面包屑导航
+// 	$(".main-hd span a:last-child").addClass('on');
 
-        
-		// 下拉菜单
-		$(".nav>ul>li>a").click(function(){
-			$(this).parent().siblings().find(".subnav").slideUp()
-			$(this).siblings(".subnav").slideToggle()
-			})
-			
-		$(".first li a").click(function(){
-			$(this).parent().siblings().find(".second").slideUp()
-			$(this).siblings(".second").slideToggle()
-		})
-		$(".second li a").click(function(){
-			$(this).parent().siblings().find(".third").slideUp()
-			$(this).siblings(".third").slideToggle()
-		})		
+// 图片列表
+	$("ul.product li").hover(function() {
+		$(this).find('.name').stop(true, true).animate({bottom:"0px"}, 300);
+	}, function() {
+		$(this).find('.name').animate({bottom:"-25px"}, 300);
+	});
 
 
-		$('.sec-nav ul li:nth-child(3n),.pro-list li:nth-child(3n),.prolist li:nth-child(3n)').css('marginRight',0)	
-		// $('.prolist li a').hover(function(){
-		// 	$(this).find('h2').animate({height:"38px"});
-		// },function(){
-		// 	$(this).find('h2').animate({height:"0px"});
+// 产品展示
+	if ($(".case-bd .case-last img").length < 2) {
+		$(".case-hd").css({display:'none'});
+	};
+	jQuery(".case-list").slide({ titCell:".case-hd ol li", mainCell:".case-bd ul", effect:"left",interTime:2500, autoPlay:true,delayTime:200, trigger:"click"});
+	//小图左滚动切换
+	jQuery(".scroll").slide({ mainCell:".case-hd ol",delayTime:100,vis:4,effect:"left",autoPage:true,scroll:4 ,pnLoop:false, autoPlay:true,interTime:7500,prevCell:".sPrev",nextCell:".sNext",pnLoop:false});
 
-		// })
-		// $('.pro-list li a,.picScroll-left .bd ul li a').hover(function() {
-		// 	$(this).children('.proimg').children('h3').show();
-		// }, function() {
-		// 	$(this).children('.proimg').children('h3').hide();
-			
-		// })
-
-})
-$(function () {
-	
-	var winw = parseInt($(window).width());
-	// $('.menu .nav .box').css({'padding-left':(winw-960)/2,'padding-right':(winw-960)/2,'margin-left':-(winw-960)/2});
-	// $('.menu .nav .box ul').css({'padding-left':(winw-960)/2,'padding-right':(winw-960)/2,'margin-left':-(winw-960)/2});
-	
-	var navLi=$(".menu .nav li");
-	navLi.mouseover(function () {
-		$(this).find("a").addClass("current");
-		$(this).find(".box").stop().slideDown(200);
-	})
-	navLi.mouseleave(function(){
-		$(this).find("a").removeClass("current");
-		$(this).find(".box").stop().slideUp(200);
-	})
-	// console.log($('.nav>li').size())
-
-})
+});
