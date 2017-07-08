e6d1d26f067a8f6f07efa336a47df4b853e9df27
bdxm <baidu@eexx.me>
2017-07-08 09:50:42 +0800

    auto

---
+++ public/templates/G0436PCN70/js/function.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {		
+
+		// banner轮播
+  	 	jQuery(".slider").slide({mainCell:".bd ul",autoPlay:true, effect:"left",delayTime:500, trigger:"click",interTime:4000});        
+
+        //首页产品分类选项卡
+        jQuery(".products").slide({titCell:".hd_pro li",mainCell:".bd_pro",trigger:"mouseover"});
+
+        //首页案例分析选项卡
+        //jQuery(".showcase").slide({titCell:".hd_ca li",mainCell:".bd_ca",trigger:"click"});
+
+        //产品详情页 多图轮播
+        jQuery(".pro_pic").slide({mainCell:".bd ul",autoPlay:true, effect:"left", trigger:"click",delayTime:500,interTime:4000});
+        
+        //回到顶部
+        $(".totop a").click(function(){
+            $("body").animate({scrollTop:0},600);
+            //console.log($("body").scrollTop())
+            return false;
+        });
+
+        //首页关于我们 企业相册
+        jQuery(".about_img_s").slide({mainCell:".bd ul",autoPlay:true,effect:"leftMarquee",vis:3,interTime:50,trigger:"click"});
+
+        //面包屑导航
+        //$(".here a:last-child").addClass('in');
+
+        //侧边菜单栏 展开
+        $(".left_list .first li a").click(function() {
+        	$(this).addClass('cu1')
+            $(this).parent().siblings().find('a').removeClass('cu1')
+            $(this).parent().siblings().find('.second a').removeClass('cu2')
+            $(this).parent().siblings().find('.second').slideUp();
+        	$(this).siblings('.second').slideToggle();
+        });
+        $(".second li a").click(function() {
+        	$(this).addClass('cu2')
+            $(this).parent().siblings().find('a').removeClass('cu2')
+            $(this).parent().siblings().find('.third').slideUp();
+        	$(this).siblings('.third').slideToggle();
+        });
+
+
+
+        //nav横向下拉菜单栏
+        $("ul.one li").hover(function(){
+        		$(this).addClass("cu")
+        		$(this).find("ul.two").stop(false,true).slideDown("fast")
+        		},function(){
+        		$(this).removeClass("cu")
+        		$(this).find("ul.two").stop(false,true).slideUp("fast")
+        		})
+
+        //字数控制
+        $(".kz").each(function(){
+          var maxwidth=parseInt($(this).attr("data-limit"));
+          if($(this).text().length>maxwidth){
+          $(this).text($(this).text().substring(0,maxwidth));
+          $(this).html($(this).html()+'...');
+          }
+        });
+
+
+
+});
