f7e311284afc9a54c04d4724a5e03473c7525b20
bdxm <baidu@eexx.me>
2017-08-09 11:45:11 +0800

    auto

---
+++ public/templates/G0371MCN50/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    
+// 图文列表
+    $('.column-press').swiper({slidesPerView: 1,paginationClickable: true,freeMode: true,autoplay:2500,nextButton: '.column-next',prevButton: '.column-prev'});
+
+
+});
